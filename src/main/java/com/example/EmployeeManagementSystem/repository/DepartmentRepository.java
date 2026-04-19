f2fe5604d6b8c8a9f085685104ef4856497c00e5
Ahmad Altawil <ahmad.altawil@stud.uni-due.de>
2026-04-19 12:06:00 +0200

    feat: add Employee, Department, Location repositories

---
+++ src/main/java/com/example/EmployeeManagementSystem/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package com.example.EmployeeManagementSystem.repository;
+
+import com.example.EmployeeManagementSystem.model.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    Optional<Department> findByName(String name);
+    boolean existsByName(String name);
+}
